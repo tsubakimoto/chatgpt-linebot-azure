fbeea5dd5f11ea6ba35b3f475594c8ab6406e883
Yuta Matsumura <tkmbmbr@gmail.com>
2023-05-06 22:44:30 +0000

    Add echo function

---
+++ ChatGPTLineBot/Echo.cs
@@ -0,0 +1,18 @@
+namespace ChatGPTLineBot;
+
+public class Echo
+{
+    private readonly ILogger<Echo> _logger;
+
+    public Echo(ILogger<Echo> logger)
+    {
+        _logger = logger;
+    }
+
+    [FunctionName("Echo")]
+    public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req)
+    {
+        _logger.LogDebug("This is debug log on Echo.");
+        return new OkObjectResult($"Now: {DateTime.UtcNow}");
+    }
+}
