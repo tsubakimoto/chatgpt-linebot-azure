c3abb80874ad6c01aa825aaeaf13c306d7e4335d
Yuta Matsumura <tkmbmbr@gmail.com>
2023-05-28 20:41:06 +0900

    Store conversations per user

--- ChatGPTLineBot/Bot.cs
+++ ChatGPTLineBot/Bot.cs
@@ -6,17 +6,15 @@ public class Bot
 {
     private static readonly string _messagingApiUrl = "https://api.line.me/v2/bot/message/reply";
     private static readonly HttpClient _httpClient = new();
-    private static readonly string _base_system_message = "You are a helpful assistant.";
-    private static readonly IList<ChatMessage> _conversations = new List<ChatMessage>();
+    private static readonly string _baseSystemMessage = "You are a helpful assistant.";
+    private static readonly ChatMessage _systemRoleChatMessage = new(ChatRole.System, _baseSystemMessage);
+    private static readonly IDictionary<string, IList<ChatMessage>> _conversations = new Dictionary<string, IList<ChatMessage>>();
 
     private readonly ILogger<Bot> logger;
 
     public Bot(ILogger<Bot> logger)
     {
         this.logger = logger;
-
-        // init chat conversations
-        _conversations.Add(new ChatMessage(ChatRole.System, _base_system_message));
     }
 
     [FunctionName("Bot")]
@@ -33,13 +31,19 @@ public class Bot
         var json = JsonSerializer.Deserialize<LineMessageReceiveJson>(requestBody);
         logger.LogDebug("Message: {0}", json.Message);
 
+        if (!_conversations.ContainsKey(json.destination))
+        {
+            _conversations.Add(json.destination, new List<ChatMessage> { _systemRoleChatMessage });
+            logger.LogDebug("Init conversation: {0}", json.destination);
+        }
+
         var channelSecret = Environment.GetEnvironmentVariable("LineChannelSecret");
         var accessToken = Environment.GetEnvironmentVariable("LineAccessToken");
 
         if (IsSignature(xLineSignature, requestBody, channelSecret)
             && json.EventType == "message")
         {
-            var result = await RunCompletionAsync(json.Message);
+            var result = await RunCompletionAsync(json.destination, json.Message);
             logger.LogDebug("ChatGPT: {0}", result);
 
             await ReplyAsync(json.ReplyToken, result, accessToken);
@@ -48,9 +52,9 @@ public class Bot
         return new BadRequestResult();
     }
 
-    private static async Task<string> RunCompletionAsync(string prompt)
+    private async Task<string> RunCompletionAsync(string userId, string prompt)
     {
-        _conversations.Add(new ChatMessage(ChatRole.User, prompt));
+        _conversations[userId].Add(new ChatMessage(ChatRole.User, prompt));
 
         var resourceName = Environment.GetEnvironmentVariable("AzureOpenAIResourceName");
         var apiKey = Environment.GetEnvironmentVariable("AzureOpenAIApiKey");
@@ -68,14 +72,17 @@ public class Bot
             FrequencyPenalty = 0,
             PresencePenalty = 0,
         };
-        foreach (var c in _conversations)
+        foreach (var c in _conversations[userId])
         {
             options.Messages.Add(c);
         }
 
         var response = await client.GetChatCompletionsAsync(deploymentName, options);
-        var content = response?.Value?.Choices?.FirstOrDefault()?.Message?.Content ?? string.Empty;
-        _conversations.Add(new ChatMessage(ChatRole.Assistant, content));
+        var choice = response?.Value?.Choices?.FirstOrDefault();
+        var content = choice?.Message?.Content ?? string.Empty;
+        _conversations[userId].Add(new ChatMessage(ChatRole.Assistant, content));
+        logger.LogDebug("Finish reason: {0}", choice?.FinishReason);
+        logger.LogDebug("Conversation count: {0}", _conversations[userId].Count);
 
         return content;
     }
